24a45a6213059a917e8ea82698b632309dcd34ac
Tejus-Gangadharaiah <45301728+Tejus-Gangadharaiah@users.noreply.github.com>
2019-03-12 23:12:46 -0600

    Update scared.c

--- scared.c
+++ scared.c
@@ -1,3 +1,27 @@
+enum scaredState(START, SK1, SK2, RETREAT);
+
+enum scaredState = START;
+
+time_t seconds = time(NULL);
+
 int scared(void){
-  return 0;
+  switch (scaredState){
+    case START:
+      
+      break;
+    case SK1:
+      
+      break;
+    case SK2:
+      
+      break;
+    case Retreat:
+      
+      break;
+    default:
+      ;
+      
+      
   }
+  return 0;
+}
